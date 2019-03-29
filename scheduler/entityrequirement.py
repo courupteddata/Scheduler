8a05c28f5b14587ec3f7ff705fbbdda1d6ea64b1
Nathan <nathan@jones.one>
2019-03-28 20:12:30 -0400

    Some code cleanup and moved onto relative.

--- scheduler/entityrequirement.py
+++ scheduler/entityrequirement.py
@@ -1,9 +1,14 @@
 from .entitystate import EntityState
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 from enum import Enum, auto
 
 """
 Contains the base requirement for units to compute cost
+
+Flow should be:
+ 1) applies(self, entity_state: EntityState, shift_start: datetime, shift_end: datetime) -> bool
+ if True:
+ 2) EntityRequirement().cost [to get the cost]
 
 different creation methods exist:
 
@@ -11,6 +16,9 @@ TimeFrameRequirement:
 1) create_day_week_requirement(label: str, start: datetime, end: datetime, cost: float)
 2) create_date_range_requirement(label: str, start: datetime, end: datetime, cost: float)
 3) create_time_range_requirement(cls, label: str, start: time, end: time, cost: float)
+
+RelativeRequirement:
+1)
 
 """
 
@@ -35,25 +43,25 @@ class EntityRequirement:
 
 class TimeFrameRequirement(EntityRequirement):
     class Types(Enum):
-        UNDEFINED = auto
-        DAY_OF_WEEK = auto
-        DATE_RANGE = auto
-        TIME_RANGE = auto
+        UNDEFINED = auto()
+        DAY_OF_WEEK = auto()
+        DATE_RANGE = auto()
+        TIME_RANGE = auto()
 
     def __init__(self, label: str, cost: float):
         super().__init__(label, cost, False)
         self.time_frame_type = TimeFrameRequirement.Types.UNDEFINED
 
         # DayOfWeek Typed
-        self.day_of_week: int = -1
+        self.day_of_week: int
 
         # DateRange Typed
-        self.datetime_start: datetime = None
-        self.datetime_end: datetime = None
+        self.datetime_start: datetime
+        self.datetime_end: datetime
 
         # TimeRange Typed
-        self.time_start: time = None
-        self.time_end: time = None
+        self.time_start: time
+        self.time_end: time
 
     @classmethod
     def create_day_week_requirement(cls, label: str, day_of_week: datetime, cost: float):
@@ -66,6 +74,7 @@ class TimeFrameRequirement(EntityRequirement):
         """
         created = cls(label, cost)
         created.day_of_week = day_of_week.weekday()
+        created.time_frame_type = TimeFrameRequirement.Types.DAY_OF_WEEK
 
         return created
 
@@ -85,6 +94,10 @@ class TimeFrameRequirement(EntityRequirement):
     @classmethod
     def create_time_range_requirement(cls, label: str, start: time, end: time, cost: float):
         # A range can span multiple days, but the importance is that it is just time
+
+        if start == end:
+            raise ValueError("start and end cannot be the same, use date range instead if needed.")
+
         created = cls(label, cost)
         created.time_frame_type = TimeFrameRequirement.Types.TIME_RANGE
         created.time_start = start
@@ -105,11 +118,31 @@ class TimeFrameRequirement(EntityRequirement):
             # (StartA <= EndB)  and  (EndA >= StartB)
             return self.datetime_start <= shift_end and self.datetime_end >= shift_start
         elif self.time_frame_type is TimeFrameRequirement.Types.TIME_RANGE:
-            # shift_start time <
-            return self.time_start <= shift_end.time() and self.time_end >= shift_start.time()
+            # 11pm to 11pm is invalid
+            """
+            start  end
+              |------|
+            what if the shift is 24 hours? this breaks, assume that one that spans 24 hours or more to encompass' time
+            86400 seconds in a day   
+            """
+            if (shift_end - shift_start).total_seconds() >= 86400.0:
+                return True
+            else:
+                return self.time_start <= shift_end.time() and self.time_end >= shift_start.time()
 
 
 class RelativeRequirement(EntityRequirement):
 
     def __init__(self, label: str, cost: float):
-        super().__init__(label, cost)
+        super().__init__(label, cost, True)
+        self.distance: timedelta
+
+    @classmethod
+    def create_relative_requirement(cls, label: str, distance: timedelta, cost: float):
+        created = cls(label, cost)
+        created.distance = distance
+
+        return created
+
+    def applies(self, entity_state: EntityState, shift_start: datetime, shift_end: datetime) -> bool:
+        return (entity_state.last_schedule - shift_start) >= self.distance

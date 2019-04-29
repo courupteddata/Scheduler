a7f9c271aeb56fabcddc5ae3cdbe0f763f0aba66
Nathan <nathan@jones.one>
2019-04-28 21:27:29 -0400

    Created empty files to start glue process between the UI and backend...

---
+++ webapi/entityapi.py
@@ -0,0 +1,23 @@
+import flask
+from flask import request, jsonify
+
+from scheduler import entitymanager
+
+app = flask.Flask(__name__)
+
+"""
+Example for something to run before 
+@app.before_first_request
+def activate_job():
+    def run_job():
+        while True:
+            print("Run recurring task")
+            time.sleep(3)
+
+    thread = threading.Thread(target=run_job)
+    thread.start()
+"""
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

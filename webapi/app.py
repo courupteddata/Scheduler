a68a6d56dcdb45d9af2429272d6cd5e0aaa7ca60
Nathan <nathan@jones.one>
2019-05-11 12:51:35 -0400

    Fixes

--- webapi/app.py
+++ webapi/app.py
@@ -7,7 +7,7 @@ app.register_blueprint(entityapi.entity_api, url_prefix=API_PREFIX)
 app.register_blueprint(locationapi.location_api, url_prefix=API_PREFIX)
 app.register_blueprint(shiftapi.shift_api, url_prefix=API_PREFIX)
 app.register_blueprint(schedulerapi.scheduler_api, url_prefix=API_PREFIX)
-app.register_blueprint(workapi.work_api, url_prefeix=API_PREFIX)
+app.register_blueprint(workapi.work_api, url_prefix=API_PREFIX)
 
 # look into https://www.getpostman.com/postman
 @app.before_first_request

743da7723263db22d320932e2d2f982370e4f99b
Nathan <nathan@jones.one>
2019-05-13 20:13:30 -0400

    Adding copyright with open source segment for backend.

--- scheduler/requirementhelper.py
+++ scheduler/requirementhelper.py
@@ -1,6 +1,26 @@
+"""
+    This file is part of Scheduler.
+
+    Scheduler is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Scheduler is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Scheduler.  If not, see <https://www.gnu.org/licenses/>.
+
+    requirementhelper.py, Copyright 2019 Nathan Jones (Nathan@jones.one)
+"""
 from typing import TYPE_CHECKING, Union, cast
 from . import entityrequirement
+
 from enum import Enum, auto
+
 import json
 
 if TYPE_CHECKING:
